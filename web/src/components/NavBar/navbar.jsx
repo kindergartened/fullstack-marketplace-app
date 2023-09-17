fd66ef2122f805d4bea06f1ecd3d626bc048823e
Pavelavl <sfdrus2004@gmail.com>
2023-09-18 00:32:36 +0300

    Navbar upd

--- web/src/components/NavBar/navbar.jsx
+++ web/src/components/NavBar/navbar.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./navbar.module.css";
+import SearchBox from "./SearchBox/SearchBox";
 
 const Navbar = () => {
+    const [showSearch, setShowSearch] = useState(false);
+
+    function onSearchBoxChanged(e) {
+        console.log(e);
+    }
+
     return (
         <div>
             <div className={styles.navbar}>
-                <span className={styles.navtext}>SNEAKERS SHOP</span>
                 <div className={styles.catalog}>
                     <button className={styles.buts} id={styles['catalog']}/>
                 </div>
+                <span className={styles.navtext}>SNEAKERS SHOP</span>
                 <div className={styles.navbut}>
+                    {showSearch ? <SearchBox onChanged={onSearchBoxChanged}/> : null}
+                    <button onClick={() => setShowSearch(prev => !prev)} className={styles.buts} id={styles['search']}/>
                     <button className={styles.buts} id={styles['favourites']}/>
                     <button className={styles.buts} id={styles['cart']}/>
                     <button className={styles.buts} id={styles['profile']}/>
-                </div>
-                <div className={styles.searchbut}>
-                    <form className={styles.searchform} action="#">
-                        <input type="text" placeholder="кеды puma" id={styles['stext']}/>
-                        <button className={styles.buts} id={styles['search']}/>
-                    </form>
                 </div>
             </div>
         </div>
