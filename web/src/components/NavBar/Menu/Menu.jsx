fbce53dac8afb41b69616dd8c6a119a14a93c5ce
Владислав <vlad2004ki@gmail.com>
2023-09-25 22:09:56 +0300

    menubut

--- web/src/components/NavBar/Menu/Menu.jsx
+++ web/src/components/NavBar/Menu/Menu.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import styles from "./Menu.module.css";
 import {CloseButton} from "react-bootstrap";
 
-const Menu = ({ isShow }) => {
-    function closeMenu() {
-        isShow = !isShow;
-    }
-
+const Menu = (isShow, setShowMenu) => {
+    const items = [{value: "Главная", href: ""}, {value: "Контакты", href: ""},
+        {value: "Мы на карте", href: ""},{value: "О нас", href: ""}]
     return (
-        <div className={styles.menu + " " + (isShow ? styles.showMenuAnimation : "")}>
-            <a href="#">Penis</a>
-            <a href="#">popa</a>
-            <a href="#">huii</a>
-
-            <span onClick={closeMenu}><CloseButton/></span>
+        <div className={styles.menu + " "+(isShow ? styles.showMenuAnimation : "")}>
+            <div className={styles.blur}/>
+            <div className={styles.menu_content} onClick={e => e.stopPropagation()}>
+                <div className={styles.menu_header}></div>
+                <ul>
+                    {items.map(item =>
+                        <li>
+                            <a href={item.href}>{item.value}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
--- web/src/components/NavBar/navbar.jsx
+++ web/src/components/NavBar/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <Menu isShow={showMenu}></Menu>
+            <Menu isShow={showMenu} setShow={setShowMenu}></Menu>
 
             <div className={styles.navbar}>
                 <div className={styles.catalog}>
