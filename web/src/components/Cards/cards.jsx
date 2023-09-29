760866d9cb1c555ee7868038effc42608fb62041
TheRomaks <niks_2004@bk.ru>
2023-09-29 21:56:07 +0300

    ссс

--- web/src/components/Cards/cards.jsx
+++ web/src/components/Cards/cards.jsx
@@ -6,14 +6,14 @@ function Cards(){
         <div>
             <Card className={styles.card}>
                 <Card.Img className={styles.image} variant="top" src="https://a.allegroimg.com/s1024/0ca2b9/7394c97f4afba79442da19512e0a" />
-                <Card.Body>
-                    <Card.Title className={styles.title}>100</Card.Title>
-                    <Card.Text className={styles.Text}>Кроссовка Рита</Card.Text>
-                    <div >
+                <Card.Body className={styles.body}>
+                    <Card.Title className={styles.title}>100 ₽</Card.Title>
+                    <Card.Text className={styles.Text}>Кроссовка Рита</Card.Text>    
+                </Card.Body>
+                    <Card.Footer className={styles.footer}>
                         <button  className={styles.addtofav + " "+styles.buts}/>
                         <button className={styles.addtocard + " "+styles.buts}/>
-                    </div>     
-                </Card.Body>
+                    </Card.Footer>
             </Card>
         </div>
     );
