1dd32d3618dc8ccb21e79630fe0df4188def601e
Pavelavl <sfdrus2004@gmail.com>
2023-10-03 21:40:34 +0300

    merge fixes
    (cherry picked from commit f1a18a6f8c1a2bff15b909ae195c5cb5acbc5f32)

--- web/src/index.js
+++ web/src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {RouterProvider} from "react-router-dom";
 import router from "./routes";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
-        <RouterProvider router={router}/>
-    </DevSupport>
+    <RouterProvider router={router}/>
 );
