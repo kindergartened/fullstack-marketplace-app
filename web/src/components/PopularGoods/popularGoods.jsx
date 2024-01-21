e139d8719a123ddfa1d3080e054e74bfa67c3e04
Vladick <vlad2004ki@gmail.com>
2024-01-21 19:51:40 +0300

    закончил штуку

--- web/src/components/PopularGoods/popularGoods.jsx
+++ web/src/components/PopularGoods/popularGoods.jsx
@@ -4,11 +4,16 @@ import {CardComponent} from "../CardList/Card/card";
 
 export const PopularGoods = () => {
     return (
-        <div className={styles.container}>
-            <CardComponent CardItem={{id: "1", head: "Кросовка Рита", price: "300р"}}/>
-            <CardComponent CardItem={{id: "2", head: "Кросовка Рита", price: "300р"}}/>
-            <CardComponent CardItem={{id: "3", head: "Кросовка Рита", price: "300р"}}/>
-            <CardComponent CardItem={{id: "4", head: "Кросовка Рита", price: "300р"}}/>
+        <div>
+            <div className={styles.text}>
+                <span>Популярно сейчас</span>
+            </div>
+            <div className={styles.container}>
+                <CardComponent CardItem={{id: "1", head: "Кросовка Рита", price: "300р"}}/>
+                <CardComponent CardItem={{id: "2", head: "Кросовка Рита", price: "300р"}}/>
+                <CardComponent CardItem={{id: "3", head: "Кросовка Рита", price: "300р"}}/>
+                <CardComponent CardItem={{id: "4", head: "Кросовка Рита", price: "300р"}}/>
+            </div>
         </div>
     );
 };
