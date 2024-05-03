83c460b5026aee050214b9840bb66c5c925f7108
Vladick <vlad2004ki@gmail.com>
2024-05-03 22:13:19 +0300

    migr upd

--- server/migration.sql
+++ server/migration.sql
@@ -33,16 +33,16 @@ create sequence carts_id_seq;
 
 
 
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (1, 'Puma', 'good puma', 100, 'https://static.street-beat.ru/upload/iblock/a47/z15gk831h2yxkz612cm7znod7vie72bv.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (2, 'Adidas', 'good abibas', 150, 'https://static.wixstatic.com/media/8f3362_d1cb37f3c1384573af6b18a9f2359ed8~mv2.jpg/v1/fill/w_600,h_527,fp_0.50_0.50,lg_1,q_80,enc_auto/8f3362_d1cb37f3c1384573af6b18a9f2359ed8~mv2.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (3, 'Nike', 'good nike', 200, 'https://outmaxshop.ru/components/com_jshopping/files/img_products/37953/nike-air-force-1-low-x-tiffany-37953-1.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (4, 'Rita', 'good rita', 177, 'https://main-cdn.sbermegamarket.ru/big1/hlr-system/199/280/973/525/029/100053095372b0.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (5, 'Demix', 'good dimask', 200, 'https://irecommend.ru/sites/default/files/product-images/932071/7QmflzG3hOfAEnEX32tBA.png', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (6, 'Outventure', 'good wintreout', 101, 'https://m.onlinetrade.ru/img/items/m/2309189_1.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (7, 'New Balance', 'good balancenew', 200, 'https://static.insales-cdn.com/images/products/1/5675/628913707/23.jpg', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (8, 'MirSever', 'good mirsever', 299, 'https://novosibirsk.novasport.ru/upload/iblock/c8d/pyyzx08u4j9posimeuj64e6ksiv2gpl2/47734.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (1, 'Puma', 'good puma', 100, 'https://a.lmcdn.ru/product/R/T/RTLADJ435101_22781336_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (2, 'Adidas', 'good abibas', 150, 'https://a.lmcdn.ru/product/R/T/RTLADG307601_23254253_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (3, 'Nike', 'good nike', 200, 'https://a.lmcdn.ru/product/R/T/RTLADK770101_23274514_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (4, 'Rita', 'good rita', 177, 'https://a.lmcdn.ru/product/R/T/RTLADK005401_22866505_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (5, 'Demix', 'good dimask', 200, 'https://a.lmcdn.ru/product/M/P/MP002XM0VR9D_22469550_2_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (6, 'Outventure', 'good wintreout', 101, 'https://a.lmcdn.ru/product/R/T/RTLADC993001_21867910_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (7, 'New Balance', 'good balancenew', 200, 'https://a.lmcdn.ru/product/R/T/RTLADE153501_22224506_3_v1.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (8, 'MirSever', 'good mirsever', 299, 'https://a.lmcdn.ru/img600x866/M/P/MP002XM0V5UZ_19374347_2_v1.jpg', '2023-01-01');
 INSERT INTO goods (id, title, description, price, img_url, created_at) values (9, 'Russia', 'good russia', 300, 'https://i.pinimg.com/originals/48/ac/30/48ac30df014bc89c13b5677c36f41386.png', '2023-01-01');
-INSERT INTO goods (id, title, description, price, img_url, created_at) values (10, 'Leopard', 'just a boot', 1, 'https://diva-charms.com/6499-superlarge_default/leopard-boots.jpg', '2023-01-01');
+INSERT INTO goods (id, title, description, price, img_url, created_at) values (10, 'Leopard', 'just a boot', 1, 'https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_65d1ec09f2867c0e5f40eb77_65d1ef81c8e97712d24e5dd4/scale_1200', '2023-01-01');
 
 
 create table favourites (
